d3ed95e93694a0d560822d2c9d0f11d53091d7c2
Scott Humphries <git@sscotth.io>
2016-06-03 12:34:45 -0500

    angular-route hello world

--- src/app/main.js
+++ src/app/main.js
@@ -1,4 +1,13 @@
-angular.module('app', [])
+angular.module('app', ['ngRoute'])
+  .config(($routeProvider) => (
+    $routeProvider
+      .when('/', {
+        controller: 'MainCtrl',
+        controllerAs: 'main',
+        templateUrl: 'app/partials/main.html',
+      })
+      .otherwise('/')
+  ))
   .controller('MainCtrl', function () {
     this.text = "Hello World"
   })
