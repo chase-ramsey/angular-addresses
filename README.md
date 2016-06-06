af6997592dc500d63a0b4f9891a2dc783d13b7ad
Chase Ramsey <ramsey.chase@gmail.com>
2016-06-06 09:44:53 -0500

    Getting setup for address project

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # angular-addresses
+
+TODO:
+ADDRESSES
+    Upload photos?
+    Table (Index of addresses)
+        Names Addresses Phone # Email, Twitter, Snapchat
+    Form (Create new address)
+        All the form fields
+    Edit (Edit Addresses)
+        All the form fields
+    Delete
+
+AUTHENTICATION
+    Login form
+    Register form
+    Logout button
+
+Landing Page
--- src/app/main.js
+++ src/app/main.js
@@ -10,18 +10,4 @@ angular.module('app', ['ngRoute', 'ui.bootstrap'])
   ))
   .controller('MainCtrl', function () {
     const main = this
-
-    main.alerts = [
-      {
-        type: 'danger',
-        msg: 'Oh snap!'
-      },
-      {
-        type: 'success',
-        msg: 'Hello World!'
-      }
-    ]
-
-    main.closeAlert = (index) => main.alerts.splice(index, 1)
-
   })
